f08201bfdfb35f0f3439634daffac5220c6ea59d
Dimitris Psaropoulos <dimitris.psaropoulos@getchute.com>
2019-07-08 17:39:44 +0200

    Add a correct SSR detection

--- src/components/Modal/Gateway.js
+++ src/components/Modal/Gateway.js
@@ -7,15 +7,21 @@ import { type ModalType } from './Modal';
 
 const FirstChild = ({ children }) => Children.toArray(children)[0] || null;
 
+const canUseDOM = !!(
+  typeof window !== 'undefined' &&
+  window.document &&
+  window.document.createElement
+);
+
 export default class ModalGateway extends Component<{
   children: ModalType,
   target: HTMLElement,
 }> {
   static defaultProps = {
-    target: (typeof(window) !== 'undefined') ? document.body : null,
+    target: canUseDOM ? document.body : null,
   };
   render() {
-    if (typeof(window) === 'undefined') return null;
+    if (!canUseDOM) return null;
     const { target, children } = this.props;
     return createPortal(
       <TransitionGroup component={FirstChild} children={children} />,
